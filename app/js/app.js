f9ed74d4ce9eb964e3a3fed1482ee84984b3f5b8
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2020-12-13 23:57:44 +0300

    add product card (html)

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,6 @@
 $(function(){
 
+	// slider
 	$('.banner-section__slider').slick({
 		dots: true,
   	slidesToShow: 1,
@@ -7,15 +8,17 @@ $(function(){
 		nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/dest/arrow-right.svg" alt=""></button>',
 	});
 
+// tabs
 	$('.tab').on('click', function(e){
-		e.preventDefault();
+		e.preventDefault();  //turn off link
 
 		$($(this).siblings()).removeClass('tab--active');
 		$($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
-		$(this).addClass('tab--active');
+		$(this).addClass('tab--active'); 
 		$($(this).attr('href')).addClass('tabs-content--active');
 	});
+
 
 	$('.products-item__favorite').on('click', function(){
 		$(this).toggleClass('products-item__favorite--active')
