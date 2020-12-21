7eb8cc61920e66264792fad0f13647997ae16097
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2020-12-21 23:00:29 +0300

    add adaptive catalog and product page

--- app/js/app.js
+++ app/js/app.js
@@ -21,10 +21,12 @@ $(function(){
 		e.preventDefault();  //turn off link
 
 		$($(this).siblings()).removeClass('tab--active');
-		$($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+		$($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content--active');
 
 		$(this).addClass('tab--active'); 
 		$($(this).attr('href')).addClass('tabs-content--active');
+
+		$('.products-slider').slick('setPosition');
 	});
 
 
@@ -112,6 +114,10 @@ $(function(){
   $('.footer__topdrop').on('click', function(){
   	$(this).next().slideToggle();
   	$(this).toggleClass('footer__topdrop--active');
+  });
+
+  $('.aside__btn').on('click', function(){
+  	$(this).next().slideToggle();
   });
 
 });
